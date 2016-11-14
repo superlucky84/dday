c6ece185443d6a1ba6f69346a3d9ebcd89c18262
kimjinwoo <superlucky84@gmail.com>
2016-11-14 20:04:36 +0900

    * added packing script

---
+++ packing-win32.js
@@ -0,0 +1,26 @@
+var packager = require('electron-packager');
+var options = {
+    'arch': 'ia32',
+    'platform': 'win64',
+    'dir': './',
+    'app-copyright': 'Superlucky',
+    'app-version': '1.1.1',
+    'asar': true,
+    'icon': './dday.ico',
+    'name': 'JW-DDAY',
+    'ignore': ['./packing', './.git'],
+    'out': './packing',
+    'overwrite': true,
+    'prune': true,
+    'version-string':{
+      'CompanyName': 'JW-DDAY',
+      'FileDescription': 'JW-DDAY', /*This is what display windows on task manager, shortcut and process*/
+      'OriginalFilename': 'JW-DDAY',
+      'ProductName': 'JW-DDAY',
+      'InternalName': 'JW-DDAY'
+    }
+};
+packager(options, function done_callback(err, appPaths) {
+    console.log(err);
+    console.log(appPaths);
+});
